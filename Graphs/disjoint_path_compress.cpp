9bdd8a9debe7f1880d9ca013aa7cdb5135bfce7d
HarikumarG <harikumarmgp@gmail.com>
2022-07-27 09:19:42 +0530

    feat[graph]: Disjoint concepts

---
+++ Graphs/disjoint_path_compress.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class UnionFind {
+    private:
+        vector<int> root;
+        int size = 0;
+    public:
+        UnionFind(int size) {
+            this->size = size;
+            root.resize(size);
+            root.shrink_to_fit();
+            for(int i = 0; i < size; i++) {
+                root[i] = i;
+            }
+        }
+        int find(int vertex) {
+            if(vertex == root[vertex])
+                return vertex;
+            return root[vertex] = find(root[vertex]);
+        }
+        bool isConnected(int vertexA, int vertexB) {
+            return find(vertexA) == find(vertexB);
+        }
+        void setUnion(int vertexA, int vertexB) {
+            int rootA = find(vertexA);
+            int rootB = find(vertexB);
+
+            if(rootA != rootB) {
+                root[rootB] = rootA;
+            }
+        }
+};
+
+int main() {
+    UnionFind uf(10);
+    // 1-2-5-6-7 3-8-9 4
+    uf.setUnion(1, 2);
+    uf.setUnion(2, 5);
+    uf.setUnion(5, 6);
+    uf.setUnion(6, 7);
+    uf.setUnion(3, 8);
+    uf.setUnion(8, 9);
+    cout << uf.isConnected(1, 5) << endl;  // true
+    cout << uf.isConnected(5, 7) << endl;  // true
+    cout << uf.isConnected(4, 9) << endl;  // false
+    // 1-2-5-6-7 3-8-9-4
+    uf.setUnion(9, 4);
+    cout << uf.isConnected(4, 9) << endl;  // true
+
+    return 0;
+}
---
+++ Graphs/disjoint_quick_union_rank_path_compress.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class UnionFind {
+    private:
+        vector<int> root;
+        vector<int> rank;
+        int size = 0;
+    public:
+        UnionFind(int size) {
+            this->size = size;
+            root.resize(size);
+            root.shrink_to_fit();
+            rank.resize(size);
+            rank.shrink_to_fit();
+            for(int i = 0; i < size; i++) {
+                root[i] = i;
+                rank[i] = 1;
+            }
+        }
+        int find(int vertex) {
+            if(vertex == root[vertex])
+                return vertex;
+            return root[vertex] = find(root[vertex]);
+        }
+        bool isConnected(int vertexA, int vertexB) {
+            return find(vertexA) == find(vertexB);
+        }
+        void setUnion(int vertexA, int vertexB) {
+            int rootA = find(vertexA);
+            int rootB = find(vertexB);
+
+            if(rootA != rootB) {
+                if(rank[rootA] > rank[rootB]) {
+                    root[rootB] = rootA;
+                } 
+                else if(rank[rootA] < rank[rootB]) {
+                    root[rootA] = rootB;
+                }
+                else {
+                    root[rootB] = rootA;
+                    rank[rootA] += 1;
+                }
+            }
+        }
+};
+
+int main() {
+    UnionFind uf(10);
+    // 1-2-5-6-7 3-8-9 4
+    uf.setUnion(1, 2);
+    uf.setUnion(2, 5);
+    uf.setUnion(5, 6);
+    uf.setUnion(6, 7);
+    uf.setUnion(3, 8);
+    uf.setUnion(8, 9);
+    cout << uf.isConnected(1, 5) << endl;  // true
+    cout << uf.isConnected(5, 7) << endl;  // true
+    cout << uf.isConnected(4, 9) << endl;  // false
+    // 1-2-5-6-7 3-8-9-4
+    uf.setUnion(9, 4);
+    cout << uf.isConnected(4, 9) << endl;  // true
+
+    return 0;
+}
